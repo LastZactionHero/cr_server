e34a7a9fcf4cf06422b72d6b6454c614ba1c49d1
Zach Dicklin <zdicklin@gmail.com>
2013-10-19 16:31:05 -0600

    Mysql

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '3.2.13'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
+gem "mysql2", "~> 0.3.13"
 
 gem "capistrano", :group => :development
 gem "rvm-capistrano", "~> 1.5.1", :group => :development
--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -2,12 +2,12 @@ class LabelsController < ApplicationController
   
   def create    
     label = Label.create_from_file(params[:image])    
-    render status: 200, json: {filename: label.filenlame}
+    render status: 200, json: {filename: label.filename}
   end
   
   def show
     label = Label.find(params[:id])    
-    render status: 200, json: {filename: label.filenlame}
+    render status: 200, json: {filename: label.filename}
   end
   
 end
