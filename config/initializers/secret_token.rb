e757c327f20ac116bb902d7a2e7b6ec7a63cac92
Zach Dicklin <zdicklin@gmail.com>
2013-10-19 16:13:49 -0600

    x

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrServer::Application.config.secret_token = '85956bcaaeffbd4afc196c8f79ff9619d82fc3569b6eb8b02e118fd6673cfae5992b9bf68f261139aebc733dbe6f86575fa44a896e11bea8629aea046207b177'
